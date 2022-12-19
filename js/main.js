340f1ba2efe29d84cec354b843af06f65efd5b51
Yousef-abdelwahed <95501008+Yousef-abdelwahed@users.noreply.github.com>
2022-12-19 19:48:59 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // ABbCDEFGHIJKLMNOpQqRSTUVWXyz
-const letters = "abcdefghijklmopqrstuvwxyz";
+const letters = "abcdefghijklmnopqrstuvwxyz";
 const letterArr= Array.from(letters.toUpperCase());
 const letterContainer = document.querySelector(".litters")
 // letterContainer.innerHTML="red"
@@ -41,18 +41,48 @@ letterAndSpace.forEach( lettSpace =>{
     }
     letterGuess.appendChild(emptySpan);
 });
-
+//wrong attempts
+let worngAttempts=0;
+const theDraw= document.querySelector(".hangman-draw")
+let guessSpan = document.querySelectorAll(".letter-guess span")
 document.addEventListener('click',(e)=>{
     if(e.target.className == "letter-box"){
+        let theStatus = false;
         e.target.classList.add("clicked");
         let clickLetter = e.target.innerHTML.toLowerCase();
-        letterAndSpace.forEach((wordLetter,index) =>{
+        let theChosenWord = Array.from(randomValueValue.toLowerCase())
+
+        theChosenWord.forEach((wordLetter,wordIndex) =>{
                 // if the clicked letter is quel one of the chosen letter
-            if(clickLetter == wordLetter){
-                console.log(index);
-            }
-        })
         
-        
+            console.log(wordLetter)
+                if(clickLetter == wordLetter){
+                    theStatus=true
+                // fill the chosen letter 
+                guessSpan.forEach((span,spanIndex)=>{
+                    if (wordIndex == spanIndex)
+                    span.innerHTML=clickLetter;
+                });
+            };   
+         });
+
+         if(theStatus !== true){
+            worngAttempts++;
+            theDraw.classList.add(`worng-${worngAttempts}`);
+         }
+         if(worngAttempts == 8){
+            letterContainer.classList.add("finished");
+            endGame();
+         }
+      
     }
-})
+});
+
+function endGame(){
+    let div = document.createElement("div");
+    let divText = document.createTextNode(`....Game over , the word is ${randomValueValue}`);
+    div.appendChild(divText);
+    div.className="popup";
+    document.body.appendChild(div);
+    console.log(randomValueValue)
+}
